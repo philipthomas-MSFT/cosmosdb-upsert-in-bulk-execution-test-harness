00bee17a149c74ab1a0b184277815993cf0c1768
philipthomas <philipthomas@microsoft.com>
2021-11-09 13:58:28 -0500

    minor formatting

--- HarnessUtility.cs
+++ HarnessUtility.cs
@@ -17,9 +17,7 @@ public class HarnessUtility
     public static async Task<string> GetCosmosDBConnectionStringAsync(string connectionName)
     {
         var tokenProvider = new AzureServiceTokenProvider();
-        var client = new KeyVaultClient(
-            authenticationCallback: new KeyVaultClient.AuthenticationCallback(tokenProvider.KeyVaultTokenCallback)
-        );
+        var client = new KeyVaultClient(authenticationCallback: new KeyVaultClient.AuthenticationCallback(tokenProvider.KeyVaultTokenCallback));
         var secret = await client.GetSecretAsync(
             vaultBaseUrl: System.Environment.GetEnvironmentVariable("HARNESS_KEY_VAULT_URL"),
             secretName: connectionName).ConfigureAwait(continueOnCapturedContext: false);
